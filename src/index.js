8d96d35c342742d5d7fc0aac8ae0dd76ed510830
Samouel <samohomiyt@gmail.com>
2022-09-15 22:24:07 +0300

    Update Color

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import animate from "./js/cubeRender.js";
-// import './styles/main.scss'
-// import './styles/main.css'
+import './styles/main.scss'
+import './styles/main.css'
 
 animate();
--- src/js/cubeRender.js
+++ src/js/cubeRender.js
@@ -4,12 +4,12 @@ import * as THREE from "three";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha:true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
